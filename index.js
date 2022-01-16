cf0af06487b42aeec21da0d444d97c302e4679c2
Fayaz <flalani100@gmail.com>
2022-01-16 14:47:32 -0500

    Generate File content is loaded

--- index.js
+++ index.js
@@ -2,3 +2,12 @@ const inquirer = require('inquirer')
 const Manager = require('./lib/Manager.js')
 const Engineer = require('./lib/Engineer.js')
 const Intern = require('./lib/Intern.js')
+const { writeFile, copyFile } = require('./lib/generateFile.js');
+const generateHTML = require('./src/html-template.js');
+let id = 0;
+
+
+function generateid() {
+    id++;
+    return id;
+}
