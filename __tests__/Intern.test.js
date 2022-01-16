314c9dfc06ad0f5d505966e970c87138013685a5
Fayaz <flalani100@gmail.com>
2022-01-16 14:35:24 -0500

    Added intern constructor and tests

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -1,10 +1,22 @@
 const Intern = require('../lib/Intern.js');
 
-test('create Engineer Object', () => {
-    const engineer = new Engineer ('Test1','1', 'email1@gmail.com', 'github1');
+test('create Intern Object', () => {
+    const intern = new Intern ('Test1','1', 'email1@gmail.com', 'school1');
 
-    expect(engineer.name).toBe('Test1');
-    expect(engineer.id).toBe('1')
-    expect(engineer.email).toBe('email1@gmail.com');
-    expect(engineer.github).toBe('github1')
+    expect(intern.name).toBe('Test1');
+    expect(intern.id).toBe('1')
+    expect(intern.email).toBe('email1@gmail.com');
+    expect(intern.school).toBe('school1')
+});
+
+test('get school information', () => {
+    const intern = new Intern ('Test2','2', 'email2@gmail.com', 'school2');
+
+    expect(intern.getSchool()).toBe('school2');
+});
+
+test('test getRole', () => {
+    const intern = new Intern ('Test3','3', 'email3@gmail.com', 'school3');
+
+    expect(intern.getRole()).toBe('Intern');
 });
