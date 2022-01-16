f31c4709df4d7238dbece53a8124aee83e0ff3dc
Fayaz <flalani100@gmail.com>
2022-01-16 14:57:57 -0500

    Added employee prompt

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const Manager = require('./lib/Manager.js')
 const Engineer = require('./lib/Engineer.js')
 const Intern = require('./lib/Intern.js')
 const { writeFile, copyFile } = require('./lib/generateFile.js');
-const generateHTML = require('./src/html-template.js');
+// const generateHTML = require('./src/html-template.js');
 let id = 0;
 
 
@@ -11,3 +11,49 @@ function generateid() {
     id++;
     return id;
 }
+
+const managerPrompt = () => {
+  
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'managerName',
+            message: "Please enter the manager's name:",
+            validate: managerName => {
+                if (managerName) {
+                    return true;
+                } else {
+                    console.log("Error - please enter manager's name")
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'managerEmail',
+            message: "Please enter the manager's email:",
+            validate: managerEmail => {
+                if (managerEmail) {
+                    return true;
+                } else {
+                    console.log("Error - please enter manager's email")
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'managerNumber',
+            message: "Please enter the manager's office number:",
+            validate: managerNumber => {
+                if (managerNumber) {
+                    return true;
+                } else {
+                    console.log("Error - please enter the manager's office number")
+                    return false;
+                }
+            }
+        }
+    ])
+}
+
